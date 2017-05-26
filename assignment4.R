19879c7d25cd29bfdef97da6417e164264c42962
daalbu <daalbu@bren.ucsb.edu>
2017-05-25 18:20:08 -0700

    lastest version 620pm!

--- assignment4.R
+++ assignment4.R
@@ -1,67 +1,71 @@
 
 
-##################Assignment 4
+##################Assignment 4##################
 
+#example prices
+salmon.price <- 1
+halibut.price <- 2
+shark.price <- 3
+tuna.price <- 4
+cod.price <- 5
+
+#example catch data
+sd.catch <- c(3,4,5,6,7)
+baja.catch <- c(3,9,5,6,7)
+sb.catch <- c(4,5,8,7,2)
+sf.catch <- c(8,4,5,6,7)
 
 #function that takes two fish tables as inputs 
-#example function
 Fishies = function(fishPrice,fishCatch) { 
   
   #user inputs for price table 
-  salmon.price <- readline(prompt="Please enter the price for salmon (in dollars): ")
-  halibut.price <- readline(prompt="Please enter the price for halibut (in dollars): ")
-  shark.price <- readline(prompt="Please enter the price for shark (in dollars): ")
-  tuna.price <- readline(prompt="Please enter the price for tuna (in dollars): ")
-  cod.price <- readline(prompt="Please enter the price for cod (in dollars): ")
+  #salmon.price <- readline(prompt="Please enter the price for salmon (in dollars): ")
+  #halibut.price <- readline(prompt="Please enter the price for halibut (in dollars): ")
+  #shark.price <- readline(prompt="Please enter the price for shark (in dollars): ")
+  #tuna.price <- readline(prompt="Please enter the price for tuna (in dollars): ")
+  #cod.price <- readline(prompt="Please enter the price for cod (in dollars): ")
   
   # make sure inputs are positive
-  if (salmon.price < 0){
-    salmon.price <- readline(prompt="Invalid price. Please enter the price for salmon (in dollars): "
-    ,salmon.price)
-  }
-  if (halibut.price < 0){
-    halibut.price <- readline(prompt="Invalid price. Please enter the price for halibut (in dollars): "
-    ,halibut.price)
-  }
-  if (shark.price < 0){
-    shark.price <- readline(prompt="Invalid price. Please enter the price for salmon (in dollars): "
-    ,shark.price)
-  }
-  if (tuna.price < 0){
-    tuna.price <- readline(prompt="Invalid price. Please enter the price for tuna (in dollars): "
-    ,tuna.price)
-  }
-  if (cod.price < 0){
-    cod.price <- readline(prompt="Invalid price. Please enter the price for cod (in dollars): "
-    ,cod.price)
-  }
+  # if (salmon.price < 0){
+  #   salmon.price <- readline(prompt="Invalid price. Please enter the price for salmon (in dollars): "
+  #                            ,salmon.price)
+  # }
+  # if (halibut.price < 0){
+  #   halibut.price <- readline(prompt="Invalid price. Please enter the price for halibut (in dollars): "
+  #                             ,halibut.price)
+  # }
+  # if (shark.price < 0){
+  #   shark.price <- readline(prompt="Invalid price. Please enter the price for salmon (in dollars): "
+  #                           ,shark.price)
+  # }
+  # if (tuna.price < 0){
+  #   tuna.price <- readline(prompt="Invalid price. Please enter the price for tuna (in dollars): "
+  #                          ,tuna.price)
+  # }
+  # if (cod.price < 0){
+  #   cod.price <- readline(prompt="Invalid price. Please enter the price for cod (in dollars): "
+  #                         ,cod.price)
+  # }
   
   # convert characters into integers
-  salmon.price <- as.integer(salmon.price)
-  halibut.price <- as.integer(halibut.price)
-  shark.price <- as.integer(shark.price)
-  tuna.price <- as.integer(tuna.price)
-  cod.price <- as.integer(cod.price)
-  
-  #price table
-  price = c(salmon.price,halibut.price,shark.price,tuna.price,cod.price)
-  fishPrice = data.frame(price=price)
-  FishSpecies <- rownames(fishPrice) <- c("salmon","halibut","shark","tuna","cod")
-  colnames(fishPrice) <- ("Price(in dollars)")
-  View(fishPrice)
+  # salmon.price <- as.integer(salmon.price)
+  # halibut.price <- as.integer(halibut.price)
+  # shark.price <- as.integer(shark.price)
+  # tuna.price <- as.integer(tuna.price)
+  # cod.price <- as.integer(cod.price)
   
   #user inputs for catch table 
-  sd.catch <- as.integer(strsplit(readline("Please enter the amount of each fish caught in San Diego 
-  (in order of salmon, halibut, shark, tuna, cod) separated by commas (example: 3,4,8,6,5): "),",")[[1]])
+  #sd.catch <- as.integer(strsplit(readline("Please enter the amount of each fish caught in San Diego 
+  #(in order of salmon, halibut, shark, tuna, cod) separated by commas (example: 3,4,8,6,5): "),",")[[1]])
   
-  baja.catch <- as.integer(strsplit(readline("Please enter the amount of each fish caught in Baja 
-  (in order of salmon, halibut, shark, tuna, cod) separated by commas (example: 3,4,8,6,5): "),",")[[1]])
+  #baja.catch <- as.integer(strsplit(readline("Please enter the amount of each fish caught in Baja 
+  #(in order of salmon, halibut, shark, tuna, cod) separated by commas (example: 3,4,8,6,5): "),",")[[1]])
   
-  sb.catch <- as.integer(strsplit(readline("Please enter the amount of each fish caught in Santa Barbara
-  (in order of salmon, halibut, shark, tuna, cod) separated by commas (example: 3,4,8,6,5): "),",")[[1]])
+  #sb.catch <- as.integer(strsplit(readline("Please enter the amount of each fish caught in Santa Barbara
+  #(in order of salmon, halibut, shark, tuna, cod) separated by commas (example: 3,4,8,6,5): "),",")[[1]])
   
-  sf.catch <- as.integer(strsplit(readline("Please enter the amount of each fish caught in San Francisco
-  (in order of salmon, halibut, shark, tuna, cod) separated by commas (example: 3,4,8,6,5): "),",")[[1]])
+  #sf.catch <- as.integer(strsplit(readline("Please enter the amount of each fish caught in San Francisco
+  #(in order of salmon, halibut, shark, tuna, cod) separated by commas (example: 3,4,8,6,5): "),",")[[1]])
   
   ########!!!!!!add error chekc for catch table inputs!!!!!
   ###if (salmon.price < 0){
@@ -69,67 +73,52 @@ Fishies = function(fishPrice,fishCatch) {
   ###,salmon.price)
   #}
   
+  #price table
+  price = c(salmon.price,halibut.price,shark.price,tuna.price,cod.price)
+  fishPrice = data.frame(price=price)
+  FishSpecies <- rownames(fishPrice) <- c("salmon","halibut","shark","tuna","cod")
+  colnames(fishPrice) <- ("Price($)")
+  View(fishPrice)
+  
   #catch table
   fishCatch = data.frame(cbind(sd.catch,baja.catch,sb.catch,sf.catch))
   colnames(fishCatch) <- c("San Diego","Baja","Santa Barbara","San Francisco")
   rownames(fishCatch) <- c("salmon","halibut","shark","tuna","cod")
   View(fishCatch)
   
-  # make sure inputs are positive
-  if (fishPrice < 0) return(NA)
-  if (fishCatch < 0) return(NA)
+  #calculate most frequently caught fish in each location
+  #need to return the fish species associated with max catch in each locaiton
+  MaxCatch <- rownames(fishCatch)[apply(fishCatch,2,which.max)]
+  print("The most frequently caught fish in San Diego, Baja, Santa Barbara, & San Francisco are as follows: ")
+  print(MaxCatch)
   
-  # calculate most frequently caught fish in each location
-  maxCatchSD <- which.max(fishCatch$`San Diego`) #returns most frequently caught fish in San Diego
-  #need to return the fish species associated with the catch not the catch value
-  return("The most frequently caught fish in San Diego was: ")
-  #return(maxCatchSD == FishSpecies)????
+  #calculate total revenue for each location, starting with San Diego
+  TotalRevenueSD <- sum(sd.catch*price)
+  TotalRevenueSD <- as.character(TotalRevenueSD)
+  print(paste0("The total revenue for San Diego is: $",TotalRevenueSD,"."))
   
-  maxCatchBaja <- which.max(fishCatch$`Baja`) #returns most frequently caught fish in Baja
-  return("The most frequently caught fish in Baja was: ")
-  return(maxCatchBaja)
+  #total revenue for Baja
+  TotalRevenueBaja <- sum(baja.catch*price)
+  print(paste0("The total revenue for Baja is: $",TotalRevenueBaja,"."))
   
-  maxCatchSB <- which.max(fishCatch$`San Barbara`) #returns most frequently caught fish in San Barbara
-  return("The most frequently caught fish in San Barbara was: ")
-  return(maxCatchSB)
+  #total revenue for Santa Barbara
+  TotalRevenueSB <- sum(sb.catch*price)
+  print(paste0("The total revenue for Santa Barbara is: $",TotalRevenueSB,"."))
   
-  maxCatchSF <- which.max(fishCatch$`San Francisco`) #returns most frequently caught fish in San Francisco
-  return("The most frequently caught fish in San Francisco was: ")
-  return(maxCatchSF)
-
-  #calculate total revenue for each location
-  totalRevenueSD <- sum(fishCatch$`San Diego`*fishPrice)
-  return("The total revenue for San Diego is: ")
-  return(totalRevenueSD)
+  #total revenue for San Francisco
+  TotalRevenueSF <- sum(sf.catch*price)
+  print(paste0("The total revenue for San Francisco is: $",TotalRevenueSF,"."))
   
-  totalRevenueBaja <- sum(fishCatch$`Baja`*fishPrice)
-  return("The total revenue for Baja is: ")
-  return(totalRevenueBaja)
+  #store all location revenues in a list
+  #Revenues <- list(SD=TotalRevenueSD,Baja=TotalRevenueBaja,SB=TotalRevenueSB,SF=TotalRevenueSF)
   
-  totalRevenueSB <- sum(fishCatch$`San Barbara`*fishPrice)
-  return("The total revenue for Santa Barbara is: ")
-  return(totalRevenueSB)
-  
-  totalRevenueSF <- sum(fishCatch$`San Francisco`*fishPrice)
-  return("The total revenue for San Francisco: ")
-  return(totalRevenueSF)
-  
-  #store all revenues in a list
-  
-  #call list to get total fisheries revenue
-  ###totalFisheriesRevenue <- sum(totalRevenueBaja )
+  #generate a total fishies sum
+  FishMoney <- c(TotalRevenueSD,TotalRevenueBaja,TotalRevenueSB,TotalRevenueSF)
+  FishMoney <- as.integer(FishMoney)
+  FishMoney <- sum(FishMoney)
+  print(paste0("The total revenue for all four locations is: $",FishMoney,"."))
 
   #user requested graph of revenvue by location
-  } 
+  #plot(aes(Revenues,))
+} 
 
-###
-# Outputs are:
-#1. most frequently caught fish in each location
-#2. total revenue for each location 
-#3. total fisheries sum 
-#4. if user requests it graph revenue by location and add total to the plot 
-# 
-# Store your function in an *.R file 
-# Generate some example data for your function; and show how this is created and used in an R markdown document 
-# 
-# Submit as usual as a git link on gauchospace
